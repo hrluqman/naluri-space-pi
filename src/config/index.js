d283a6bd2dd2586852d650f71501cc0bb0cdecde
hrluqman <hrluqman2@gmail.com>
2025-08-24 23:39:12 +0800

    feat: Update iteration interval in config

--- src/config/index.js
+++ src/config/index.js
@@ -7,5 +7,5 @@ module.exports = {
   // Persisted state JSON file path (project-root/state/state.json)
   stateFilePath: path.join(process.cwd(), 'state', 'state.json'),
   // Interval for Pi iterations (milliseconds). Increase/decrease for speed.
-  iterationIntervalMs: 1000
+  iterationIntervalMs: 2000
 };
